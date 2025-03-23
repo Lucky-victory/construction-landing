89425ab5789ba1fba9066aed4ac97bedfcf55253
Lucky Victory <luckyvictory54@gmail.com>
2025-03-23 15:06:20 +0100

    update metadata for Buildcraft construction company in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,45 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+  title: "Buildcraft construction company",
+  description:
+    "Buildcraft construction company is a construction company that specializes in building houses, offices, and other structures.",
+  keywords: "construction, building, houses, offices, structures",
+  authors: [
+    {
+      name: "Buildcraft construction company",
+      url: "https://construction-landing,vercel.app",
+    },
+  ],
+  creator: "Buildcraft construction company",
+  publisher: "Buildcraft construction company",
+  robots: "index, follow",
+  openGraph: {
+    title: "Buildcraft construction company",
+    description:
+      "Buildcraft construction company is a construction company that specializes in building houses, offices, and other structures.",
+    url: "https://construction-landing,vercel.app",
+    siteName: "Buildcraft construction company",
+    images: [
+      {
+        url: "https://construction-landing,vercel.app/images/og.png",
+        width: 1200,
+        height: 630,
+        type: "image/png",
+      },
+    ],
+  },
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
